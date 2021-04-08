8f5ea4d8a77e41e1b77bc1b2fa696af70c1decbb
Woody Kromar <wkromar12@gmail.com>
2021-04-08 17:09:56 -0500

    completed basic arrays

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] luckyNumbers = {4, 8, 15, 16, 23, 42};
+            // originally this would print 8
+            luckyNumbers[1] = 900;
+            // now it prints 900
+            Console.WriteLine(luckyNumbers[1] );
+            Console.ReadLine();
+
+            // array of strings
+            string[] friends = new string[3];
+            //number in new string tells c# how many elements the array can hold
+            // can hold 3 values
+            friends[0] ="Jim";
+            friends[1] ="Kelly";
+        }
+    }
+}
